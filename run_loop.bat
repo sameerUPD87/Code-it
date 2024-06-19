277861e1b1b3f8885b7b6127b2e2c4c23ea5be2a
sameerUPD87 <sameerupadhyay2100@gmail.com>
2024-06-19 09:58:45 +0530

    loop try2

--- run_loop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:loop
-node app.js
-REM Add a timeout if needed to avoid immediate rerun
-timeout /t 1 /nobreak >nul
-goto loop
---
+++ run_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+    node app.js
+    # Add a sleep interval if needed to avoid immediate rerun
+    sleep 1
+done
